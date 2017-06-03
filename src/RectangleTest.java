5b1e64f89218cf684d526b27ab64cdbd1bb0736a
skkandi <skkacct@gmail.com>
2017-06-03 01:44:33 -0500

    adding junit

---
+++ src/RectangleTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RectangleTest {
+  Rectangle myRectangle = new Rectangle(6,7);
+
+  @Test
+  public void testGetArea() {
+    assertEquals (myRectangle.getArea(),35);
+  }
+
+  @Test
+  public void testGetPerimeter() {
+    assertEquals (myRectangle.getPerimeter(),28);
+  }
+
+  @Test
+  public void testLength() {
+    assertEquals (myRectangle.length,6);
+  }
+
+  @Test
+  public void testWidth() {
+    assertEquals (myRectangle.width,7);
+  }
+
+
+}
